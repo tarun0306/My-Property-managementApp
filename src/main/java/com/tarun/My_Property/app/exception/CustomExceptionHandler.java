8750e932187184442b781bb0e91a9c1098da1838
tarun0306 <98694423+tarun0306@users.noreply.github.com>
2025-09-26 09:54:07 -0500

    Implemented Logback and slf4j

--- src/main/java/com/tarun/My_Property/app/exception/CustomExceptionHandler.java
+++ src/main/java/com/tarun/My_Property/app/exception/CustomExceptionHandler.java
@@ -3,6 +3,8 @@ package com.tarun.My_Property.app.exception;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -13,7 +15,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class CustomExceptionHandler {
 	
-	
+	private final Logger logger = LoggerFactory.getLogger(CustomExceptionHandler.class);
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<List<ErrorModel>> handleFieldValidation(MethodArgumentNotValidException manv) {
@@ -23,7 +25,8 @@ public class CustomExceptionHandler {
 		List<FieldError> fieldErrorList = manv.getBindingResult().getFieldErrors();
 		
 		for(FieldError fe :fieldErrorList) {
-			
+			logger.debug("Inside field Validation {} : {}", fe.getField(),fe.getDefaultMessage());
+			logger.info("Inside field Validation {} : {}", fe.getField(),fe.getDefaultMessage());
 			errorModel = new ErrorModel();
 			errorModel.setCode(fe.getCode());
 			errorModel.setMessage(fe.getDefaultMessage());
@@ -40,9 +43,13 @@ public class CustomExceptionHandler {
 	
 	@ExceptionHandler(BusinessException.class)
 	public ResponseEntity<List<ErrorModel>> handleBusinessException(BusinessException bex){
+		for(ErrorModel em : bex.getErrors()) {
+			logger.debug("BusinessException thrown  level - debug{} : {}",em.getCode(),em.getMessage());
+			logger.info("BusinessException thrown {} level - info : {}",em.getCode(),em.getMessage());
+			logger.warn("BusinessException thrown  level - warn {} : {}",em.getCode(),em.getMessage());
+			logger.error("BusinessException thrown  level-error {} : {}",em.getCode(),em.getMessage());
+		}		
 		
-		
-		System.out.println("BusinessException is thrown");
 		return new ResponseEntity<List<ErrorModel>>(bex.getErrors(),HttpStatus.BAD_REQUEST);
 		
 	}
