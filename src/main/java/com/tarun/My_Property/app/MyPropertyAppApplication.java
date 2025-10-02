2d49d7b1767bf8afca34d0c1ee64a69618f79b75
tarun0306 <98694423+tarun0306@users.noreply.github.com>
2025-10-02 13:36:17 -0500

    Tried swagger implementation and  updated

--- src/main/java/com/tarun/My_Property/app/MyPropertyAppApplication.java
+++ src/main/java/com/tarun/My_Property/app/MyPropertyAppApplication.java
@@ -1,14 +1,21 @@
 package com.tarun.My_Property.app;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
 @SpringBootApplication
+
 public class MyPropertyAppApplication {
 
 	public static void main(String[] args) {
-
+		
 		SpringApplication.run(MyPropertyAppApplication.class, args);
+		
+		
 	}
 
 
---
+++ src/main/java/com/tarun/My_Property/app/config/SwaggerConfig.java
@@ -0,0 +1,12 @@
+package com.tarun.My_Property.app.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer{
+	
+	
+}
